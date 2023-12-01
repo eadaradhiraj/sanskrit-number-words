53ced2be8e00152a5ebbc692a3756b470fa2462a
Dhiraj Eadara <eadaradhiraj@gmail.com>
2023-12-01 10:02:18 +0530

    Fix: test case formatting

--- tests/test_breaks.py
+++ tests/test_breaks.py
@@ -1,149 +1,73 @@
 import unittest
 from sanskrit_number_words.break_words import BreakWords
 
+
 class TestCases(unittest.TestCase):
     def test_breaks(self):
         self.assertEqual(
-            BreakWords.get_words(
-                "navanavatyuttaranavazatottaranavasahasra"
-            ),
-            ['navanavati', '+', 'nava', '100', '+', 'nava', '1000']
+            BreakWords.get_words("navanavatyuttaranavazatottaranavasahasra"),
+            ["navanavati", "+", "nava", "100", "+", "nava", "1000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "navanavatyadhikanavazatottaranavasahasra"
-            ),
-            ['navanavati', '+', 'nava', '100', '+', 'nava', '1000']
+            BreakWords.get_words("navanavatyadhikanavazatottaranavasahasra"),
+            ["navanavati", "+", "nava", "100", "+", "nava", "1000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "navanavatyadhikanavazataadhikanavasahasra"
-            ),
-            ['navanavati', '+', 'nava', '100', '+', 'nava', '1000']
+            BreakWords.get_words("navanavatyadhikanavazataadhikanavasahasra"),
+            ["navanavati", "+", "nava", "100", "+", "nava", "1000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "navanavatyadhikanavazatottaranavasahasra"
-            ),
-            ['navanavati', '+', 'nava', '100', '+', 'nava', '1000']
+            BreakWords.get_words("navanavatyadhikanavazatottaranavasahasra"),
+            ["navanavati", "+", "nava", "100", "+", "nava", "1000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "saptaaziityadhikanavazataadhikanavasahasra"
-            ),
-            ['saptaaziiti', '+', 'nava', '100', '+', 'nava', '1000']
+            BreakWords.get_words("saptaaziityadhikanavazataadhikanavasahasra"),
+            ["saptaaziiti", "+", "nava", "100", "+", "nava", "1000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "navanavatyadhikaikazataadhikaikasahasra"
-            ),
-            ['navanavati', '+', 'eka', '100', '+', 'eka', '1000']
+            BreakWords.get_words("navanavatyadhikaikazataadhikaikasahasra"),
+            ["navanavati", "+", "eka", "100", "+", "eka", "1000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "saptottarazatam"
-            ),
-            ['sapta', '+', '100', 'm']
+            BreakWords.get_words("saptottarazatam"), ["sapta", "+", "100", "m"]
+        )
+        self.assertEqual(BreakWords.get_words("saptazata"), ["sapta", "100"])
+        self.assertEqual(BreakWords.get_words("SoDaza"), ["16"])
+        self.assertEqual(BreakWords.get_words("saptottarazata"), ["sapta", "+", "100"])
+        self.assertEqual(
+            BreakWords.get_words("saptasaptatizata"), ["saptasaptati", "100"]
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "saptazata"
-            ),
-            ['sapta', '100']
+            BreakWords.get_words("saptasaptatizatakoTi"),
+            ["saptasaptati", "100", "10000000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "SoDaza"
-            ),
-            ['16']
+            BreakWords.get_words("saptasaptatyuttarazatakoTi"),
+            ["saptasaptati", "+", "100", "10000000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "saptottarazata"
-            ),
-            ['sapta', '+','100']
+            BreakWords.get_words("saptaaziityadhikanavazataadhikanavasahasra"),
+            ["saptaaziiti", "+", "nava", "100", "+", "nava", "1000"],
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "saptasaptatizata"
-            ),
-            ['saptasaptati','100']
+            BreakWords.get_words("ekaannasahasra"), ["eka", "minus", "1000"]
+        )
+        self.assertEqual(BreakWords.get_words("ekonasahasra"), ["eka", "minus", "1000"])
+        self.assertEqual(BreakWords.get_words("ekonaayuta"), ["eka", "minus", "10000"])
+        self.assertEqual(
+            BreakWords.get_words("ekaannaayuta"), ["eka", "minus", "10000"]
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "saptasaptatizatakoTi"
-            ),
-            ['saptasaptati','100', '10000000']
+            BreakWords.get_words("ekonaarbuda"), ["eka", "minus", "1000000000"]
+        )
+        self.assertEqual(BreakWords.get_words("Unasahasra"), ["minus", "1000"])
+        self.assertEqual(
+            BreakWords.get_words("paJcaannasahasra"), ["paJca", "minus", "1000"]
         )
         self.assertEqual(
-            BreakWords.get_words(
-                "saptasaptatyuttarazatakoTi"
-            ),
-            ['saptasaptati', '+', '100', '10000000']
+            BreakWords.get_words("paJconasahasra"), ["paJca", "minus", "1000"]
         )
+        self.assertEqual(BreakWords.get_words("caturayuta"), ["catur", "10000"])
         self.assertEqual(
-            BreakWords.get_words(
-                "saptaaziityadhikanavazataadhikanavasahasra"
-            ),
-            ['saptaaziiti', '+', 'nava', '100', '+', 'nava', '1000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "ekaannasahasra"
-            ),
-            ['eka', 'minus', '1000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "ekonasahasra"
-            ),
-            ['eka', 'minus', '1000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "ekonaayuta"
-            ),
-            ['eka', 'minus', '10000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "ekaannaayuta"
-            ),
-            ['eka', 'minus', '10000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "ekonaarbuda"
-            ),
-            ['eka', 'minus', '1000000000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "Unasahasra"
-            ),
-            ['minus', '1000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "paJcaannasahasra"
-            ),
-            ['paJca', 'minus', '1000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "paJconasahasra"
-            ),
-            ['paJca', 'minus', '1000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "caturayuta"
-            ),
-            ['catur', '10000']
-        )
-        self.assertEqual(
-            BreakWords.get_words(
-                "caturUnaayuta"
-            ),
-            ['catur', 'minus', '10000']
+            BreakWords.get_words("caturUnaayuta"), ["catur", "minus", "10000"]
         )
--- tests/test_convert_numbers_from_roman.py
+++ tests/test_convert_numbers_from_roman.py
@@ -4,178 +4,62 @@ from sanskrit_number_words import ConvertNumbers
 
 class TestCases(unittest.TestCase):
     def test_convert_numbers(self):
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="ekaannazata"), 99)
         self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="ekaannazata"
-            ),
-            99
+            ConvertNumbers.convert_from_roman(inp_str="ekaannaayuta"), 9999
         )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="ekaannaayuta"
-            ),
-            9999
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="ekonaayuta"
-            ),
-            9999
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="Unaayuta"
-            ),
-            9999
-        )
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="ekonaayuta"), 9999)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="Unaayuta"), 9999)
         self.assertEqual(
             ConvertNumbers.convert_from_roman(
                 inp_str="navanavatyadhikanavazataadhikanavasahasra"
             ),
-            9999
+            9999,
         )
         self.assertEqual(
             ConvertNumbers.convert_from_roman(
                 inp_str="navanavatyuttaranavazatottaranavasahasra"
             ),
-            9999
+            9999,
         )
         self.assertEqual(
             ConvertNumbers.convert_from_roman(
                 inp_str="aSTaaziityadhikaaSTazataadhikaaSTasahasra"
             ),
-            8888
+            8888,
+        )
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="SaTzatam"), 600)
+        self.assertEqual(
+            ConvertNumbers.convert_from_roman(inp_str="SaDuttarazatam"), 106
         )
         self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="SaTzatam"
-            ),
-            600
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="SaDuttarazatam"
-            ),
-            106
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="SaDadhikazatam"
-            ),
-            106
+            ConvertNumbers.convert_from_roman(inp_str="SaDadhikazatam"), 106
         )
         self.assertEqual(
             ConvertNumbers.convert_from_roman(
                 inp_str="saptaaziityadhikanavazataadhikanavasahasra"
             ),
-            9987
+            9987,
         )
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="paJcaannazata"), 95)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="paJconazata"), 95)
         self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="paJcaannazata"
-            ),
-            95
+            ConvertNumbers.convert_from_roman(inp_str="tryadhikazatam"), 103
         )
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="tryUnazatam"), 97)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="tryUnazatam"), 97)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="tisraH"), 3)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="trINi"), 3)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="trayaH"), 3)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="UnaviMzat"), 19)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="zatonazata"), 0)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="SoDaza"), 16)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="UnatriMzat"), 29)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="trizatakam"), 300)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="zatatrayam"), 300)
         self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="paJconazata"
-            ),
-            95
+            ConvertNumbers.convert_from_roman(inp_str="zatazatakam"), 10000
         )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="tryadhikazatam"
-            ),
-            103
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="tryUnazatam"
-            ),
-            97
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="tryUnazatam"
-            ),
-            97
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="tisraH"
-            ),
-            3
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="trINi"
-            ),
-            3
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="trayaH"
-            ),
-            3
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="UnaviMzat"
-            ),
-            19
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="zatonazata"
-            ),
-            0
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="SoDaza"
-            ),
-            16
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="UnatriMzat"
-            ),
-            29
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="trizatakam"
-            ),
-            300
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="zatatrayam"
-            ),
-            300
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="zatazatakam"
-            ),
-            10000
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="zatacatuSkam"
-            ),
-            400
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="zatapaJcakam"
-            ),
-            500
-        )
-        self.assertEqual(
-            ConvertNumbers.convert_from_roman(
-                inp_str="zatadvayam"
-            ),
-            200
-        )
-
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="zatacatuSkam"), 400)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="zatapaJcakam"), 500)
+        self.assertEqual(ConvertNumbers.convert_from_roman(inp_str="zatadvayam"), 200)
