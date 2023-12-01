28700e4d5b71237a68ebbe8ebd8ba88aaadabd2b
Dhiraj Eadara <eadaradhiraj@gmail.com>
2023-12-01 10:35:55 +0530

    FIX: setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sanskrit-number-words',
+    version='0.0.1',
+    # packages=['sanskrit_number_words.ConvertNumbers'],
+    packages=find_packages('sanskrit_number_words', exclude=['test']),
+    package_dir = {'': 'sanskrit_number_words'},
+    url="https://github.com/eadaradhiraj/sanskrit-number-words",
+    license='',
+    author='Dhiraj Eadara',
+    author_email='eadaradhiraj@gmail.com',
+    description='Convert Sanskrit number words into actual numbers'
+)
